c4c1291fca0c5428e98546dcd53ddc61831453cd
Venomv252 <aryanraj9931614200@gmail.com>
2025-05-09 13:52:30 +0530

    start

---
+++ src/Nav.jsx
@@ -0,0 +1,19 @@
+import "./nav.css";
+
+export default function Nav() {
+  return (
+    <>
+      <nav>
+        <a  id ="logo" href="#"><span id="logo">PrediPort</span></a>
+        <div class="menu">
+          <a href="#">Signup</a>
+          <a href="#">About</a>
+          <a href="#">Products</a>
+          <a href="#">Pricing</a>
+          <a href="#">Support</a>
+        </div>
+      
+      </nav>
+    </>
+  );
+}
